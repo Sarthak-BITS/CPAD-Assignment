f5c16f17ba3d56bb56eab6b04640ee1ea5512c05
Sarthak Sahu <32789001+Sarthak99@users.noreply.github.com>
2022-04-24 19:45:22 +0530

    First code push

---
+++ booking/src/test/java/com/welcomehome/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welcomehome.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
